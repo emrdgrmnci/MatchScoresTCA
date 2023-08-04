bb2bbe47d0f71648dc2827590556fb0c32ad10eb
Emre <emre.degirmenci@sisal.com>
2023-08-04 18:39:18 +0300

    Initial Commit

---
+++ MatchScoresTCA/MatchScoresTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchScoresTCAApp.swift
+//  MatchScoresTCA
+//
+//  Created by emre.degirmenci on 4.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MatchScoresTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
